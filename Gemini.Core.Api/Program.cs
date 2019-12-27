202e3b70aad3867ad5b3cd3e80cceaceea665454
Maaici <sj5678910@gmail.com>
2019-12-27 12:17:40 +0800

    添加serilog

--- Gemini.Core.Api/Program.cs
+++ Gemini.Core.Api/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using System.IO;
 
 namespace Gemini.Core.Api
 {
@@ -14,6 +17,14 @@ namespace Gemini.Core.Api
         /// <param name="args"></param>
         public static void Main(string[] args)
         {
+            //第三方log工具serilog的相关配置
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File(Path.Combine("logs", @"log.txt"), rollingInterval: RollingInterval.Day)
+                .CreateLogger();
             CreateHostBuilder(args).Build().Run();
         }
 
@@ -26,6 +37,7 @@ namespace Gemini.Core.Api
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseSerilog();
                     webBuilder.UseStartup<Startup>();
                 });
     }
